aae89d6ede89d7265009481a588795453a854b4d
hannikagabrielsson <r.hannika@gmail.com>
2023-10-25 19:46:09 +0300

    osa 1.1. valmis

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -1,3 +1,43 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p>
+        {props.course}
+      </p>
+    </>
+  )  
+}
+
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+    <p>
+      {props.osa} {props.tehtava}
+    </p>
+    {/* <p>
+      {props.part2} {props.excercises2}
+    </p>
+    <p>
+      {props.part3} {props.excercises3}
+    </p> */}
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <>
+    <p>
+    Number of exercises {props.tehtava}
+    </p>
+    </>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -6,20 +46,16 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content osa={part1} tehtava={exercises1} />
+      <Content osa={part2} tehtava={exercises2} />
+      <Content osa={part3} tehtava={exercises3} />
+      <Total tehtava={exercises1 + exercises2 + exercises3}/>
+      {/* <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
     </div>
   )
 }
